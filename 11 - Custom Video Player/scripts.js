90935bb1a6840a0f4051e962edecd5205bc03413
danbiilee <danbi.db@gmail.com>
2020-05-12 08:40:25 +0900

    0512-Completed: 11 - Custom Video Player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,21 +5,27 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const full = player.querySelector('.full');
 
 
 function togglePlay(){
+	// if(video.paused){
+	// 	video.play();
+	// } else{
+	// 	video.pause();
+	// }
+
 	const method = video.paused ? 'play' : 'pause';
-	video[method]();
+	video[method](); 
+
+	//video[video.paused ? 'play' : 'pause']();
 }
 
 function updateButton(){
 	const icon = this.paused ? '►' : '❚ ❚';
 	toggle.innerHTML = icon;
-}
 
-function handleProgress(){
-	const percent = (video.currentTime / video.duration) * 100;
-	progressBar.style.flexBasis = `${percent}%`;
+	//toggle.textContent = this.paused ? '►' : '❚ ❚';
 }
 
 function skip(){
@@ -30,11 +36,24 @@ function handleRangeUpdate(){
 	video[this.name] = this.value;
 }
 
+function handleProgress(){
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+}
+
 function scrub(e) {
 	console.log(mousedown);
+	// 영상너비 대비 내가 클릭한 위치만큼 비디오 영상의 시간을 구해서 대입 
 	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
 	video.currentTime = scrubTime;
 }
+
+function getfullScreen(){
+	// player.style.width = window.outerWidth;
+	// player.style.height = window.outerHeight;
+	player.requestFullscreen();
+}
+
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -45,6 +64,7 @@ toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+full.addEventListener('click', getfullScreen);
 
 let mousedown = false;
 progress.addEventListener('click', scrub);
