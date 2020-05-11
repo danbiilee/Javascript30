37a9ff5a19d2632828f41232780fcce563ccee45
danbiilee <danbi.db@gmail.com>
2020-05-11 20:07:48 +0900

    0511-Progressing: 11 - Custom video Player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -13,15 +13,43 @@ function togglePlay(){
 }
 
 function updateButton(){
-	console.log(this.paused);
 	const icon = this.paused ? '►' : '❚ ❚';
 	toggle.innerHTML = icon;
 }
 
+function handleProgress(){
+	const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+}
+
+function skip(){
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+	video[this.name] = this.value;
+}
+
+function scrub(e) {
+	console.log(mousedown);
+	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+// 마우스를 누른채 드래그 하는 경우에만 아래 이벤트가 발생함
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e)); 
 
